eb292a5c9114dd82274286fb8cd3d6ffe521d03e
Keijiro Takahashi <keijiro@gmail.com>
2017-07-23 01:17:53 +0900

    Particle system demo

--- Assets/Test/ParticleController.cs
+++ Assets/Test/ParticleController.cs
@@ -6,9 +6,12 @@ public class ParticleController : MonoBehaviour
 
     ParticleSystem.EmissionModule _emission;
     ParticleSystem.ShapeModule _shape;
+    ParticleSystem.NoiseModule _noise;
+    ParticleSystem.ExternalForcesModule _force;
 
     float _originalEmission;
     float _originalRadius;
+    float _originalNoise;
 
     void Start()
     {
@@ -19,11 +22,18 @@ public class ParticleController : MonoBehaviour
 
         _shape = ps.shape;
         _originalRadius = _shape.radius;
+
+        _noise = ps.noise;
+        _originalNoise = _noise.strength.constant;
+
+        _force = ps.externalForces;
     }
 
     void Update()
     {
         _emission.rateOverTime = Mathf.Clamp01(throttle * 5) * _originalEmission;
         _shape.radius = throttle * _originalRadius;
+        _noise.strength = Mathf.Clamp01(throttle + 0.5f) * _originalNoise;
+        _force.multiplier = throttle * 2 - 1;
     }
 }
